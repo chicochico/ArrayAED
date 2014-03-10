32d9acee90a4d3f49e283f0e49a9d33ecfe9eae3
Chico <fxchiang@gmail.com>
2014-03-10 01:47:33 -0300

    First commit.

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 10, 2014
+ *      Author: Francisco
+ */
+
+
+#include <stdio.h>
+
+int arrayMax(int *array, int lenght) {
+	int maxIndex = 0;
+	int i;
+
+	for (i = 1; i < lenght; i++) {
+		if (array[maxIndex] < array[i])
+			maxIndex = i;
+	}
+
+	return maxIndex;
+}
+
+int main() {
+	int array[] = {4,1,2,3};
+	int length = sizeof(array)/sizeof(int);
+	printf("%d", arrayMax(array, length));
+	return 0;
+}
